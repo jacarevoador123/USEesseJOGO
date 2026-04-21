100f1551d375295c228c6e21040042cd502cd4db
MiguelBocaLucca09 <271781445+jacarevoador123@users.noreply.github.com>
2026-04-21 14:14:06 -0300

    Criação de inimigo

---
+++ Assets/Scripts/AtaqueJogador.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class AtaqueJogador : MonoBehaviour
+{
+    [SerializeField] int danoJogador = 50;
+
+    public void DefinirDano(int novoDano)
+    {
+        danoJogador = novoDano;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Inimigo"))
+        {
+            var vidaInimigo = other.GetComponent<SistemaDeVidaInimigo>();
+            if (vidaInimigo != null)
+                vidaInimigo.AplicarDano(danoJogador);
+
+            return;
+        }
+    }
+}
